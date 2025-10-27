8552ce907d82a01c6bab7d2ec724a2dae1269756
aysintumay <aysintumay@gmail.com>
2025-10-27 21:23:38 +0000

    itignore update and static_fns clean up

--- cormpo/static_fns/Abiomed.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class StaticFns:
-    @staticmethod
-    def termination_fn(obs, act, next_obs):
-        #no termination policy
-        done = np.array([False]*obs.shape[0])
-        return done
